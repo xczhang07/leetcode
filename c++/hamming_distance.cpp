47063b020bac7457db9e7337c224a75784ce1115
zhangxc <xczhang07@gmail.com>
2017-05-20 18:40:52 -0700

    c++ solution for leetcode

---
+++ c++/hamming_distance.cpp
@@ -0,0 +1,15 @@
+int hammingDistance(int x, int y) {
+        int ret = 0;
+        int val1 = 0;
+        int val2 = 0;
+        while(x != 0 || y != 0)
+        {
+            val1 = x & 1;
+            val2 = y & 1;
+            if(val1 != val2)
+                ++ret;
+            x = x >> 1;
+            y = y >> 1;
+        }
+        return ret;
+    }
