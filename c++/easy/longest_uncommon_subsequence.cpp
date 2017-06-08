4939222923f4c829310c87d9aa2886ebc9af0769
xc-zhang <xczhang07@gmail.com>
2017-06-08 01:17:25 -0700

    Create longest_uncommon_subsequence.cpp

---
+++ c++/easy/longest_uncommon_subsequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+        if(a == b)
+            return -1;
+        else
+            return max(a.size(), b.size());
+    }
+};
+
+
+Conclusion:
+
+Too easy...no comments
