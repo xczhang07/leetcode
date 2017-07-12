9cd9ea813102325fcb10f8fdf2a4a324c9168302
xc-zhang <xczhang07@gmail.com>
2017-07-11 17:45:40 -0700

    Create sum_of_square_numbers.py

---
+++ python/easy/sum_of_square_numbers.py
@@ -0,0 +1,12 @@
+import math
+class Solution(object):
+    def judgeSquareSum(self, c):
+        """
+        :type c: int
+        :rtype: bool
+        """
+        for i in range(int(math.sqrt(c))+1):
+            if math.sqrt(c - i*i).is_integer():
+                return True
+        return False
+        
