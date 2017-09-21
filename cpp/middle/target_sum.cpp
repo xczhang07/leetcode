663bebd2571c62a07557edaa99b9b05bfb9f2baa
xc-zhang <xczhang07@gmail.com>
2017-09-20 20:05:12 -0700

    Create target_sum.cpp

---
+++ cpp/middle/target_sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int count = 0;
+        int sum = 0;
+        if(nums.size() == 0)
+            return count;
+        getCount(nums, S, count, 0, sum);
+        return count;
+    }
+    
+    void getCount(vector<int>& nums, int& target, int& count, int idx, int sum)
+    {
+        if(idx == nums.size())
+        {
+            if(sum == target)
+                ++count;
+            return;
+        }
+        getCount(nums, target, count, idx+1, sum+nums[idx]);
+        getCount(nums, target, count, idx+1, sum-nums[idx]);
+        return;
+    }
+};
+
+
+Conclusion:
+
+解法和combination特别相像， dfs。
