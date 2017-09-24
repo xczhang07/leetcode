806af6ea3636eed5d197e67d4e28594d227db246
xc-zhang <xczhang07@gmail.com>
2017-09-23 21:01:54 -0700

    Create remove_duplicates_from_sorted_array_ii.cpp

---
+++ cpp/middle/remove_duplicates_from_sorted_array_ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() == 0 || nums.size() == 1)
+            return nums.size();
+        int len = 0;
+        int count = 1;
+        for(int i = 1; i < nums.size(); ++i)
+        {
+            if(nums[i] == nums[len])
+            {
+                if(count == 2)
+                    continue;
+                else
+                    count += 1;
+            }
+            else
+                count = 1;
+            len += 1;
+            nums[len] = nums[i];
+        }
+        return len+1;
+    }
+};
+
+Conclusion:
+不是一道难题，设一个count记录下来出现次数，如果已经到达2这个值了，skip，如果没达到，count自增1.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
