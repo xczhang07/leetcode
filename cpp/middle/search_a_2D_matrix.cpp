768427a8516c20fa367f5b9f8e6e7ea2b3f4ece9
xc-zhang <xczhang07@gmail.com>
2017-09-29 20:30:05 -0700

    Update search_a_2D_matrix.cpp

--- cpp/middle/search_a_2D_matrix.cpp
+++ cpp/middle/search_a_2D_matrix.cpp
@@ -24,3 +24,34 @@ Conclusion:
 
 Time Complexity: O(logn)
 Space Complexity: O(1)
+
+Another solution:
+class Solution {
+public:
+    /*
+     * @param matrix: matrix, a list of lists of integers
+     * @param target: An integer
+     * @return: a boolean, indicate whether matrix contains target
+     */
+    bool searchMatrix(vector<vector<int>> &matrix, int target) {
+        // write your code here
+        if(matrix.size() == 0 || matrix[0].size() == 0)
+            return false;
+        int start = 0;
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int end = row*col - 1;
+        while(start <= end)
+        {
+            int mid = start + (end - start)/2;
+            int number = matrix[mid/col][mid%col];
+            if(number == target)
+                return true;
+            else if(number > target)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return false;
+    }
+};
