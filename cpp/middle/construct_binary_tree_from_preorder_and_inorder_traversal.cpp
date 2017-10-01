1fb186b72084dadffbfdb8547c566e206f73541e
xc-zhang <xczhang07@gmail.com>
2017-10-01 11:23:58 -0700

    Create construct_binary_tree_from_preorder_and_inorder_traversal.cpp

---
+++ cpp/middle/construct_binary_tree_from_preorder_and_inorder_traversal.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if(preorder.size() != inorder.size())
+            return NULL;
+        else if(inorder.size() == 0)
+            return NULL;
+        return constructBinaryTree(inorder, 0, inorder.size()-1, preorder, 0, preorder.size()-1);
+    }
+    
+    TreeNode* constructBinaryTree(vector<int>& inorder, int i_st, int i_end, vector<int>& preorder, int p_st, int p_end)
+    {
+        if(i_st > i_end || p_st > p_end)
+            return NULL;
+        TreeNode* root = new TreeNode(preorder[p_st]);
+        int root_idx = getRootIndex(inorder, preorder[p_st]);
+        int left_size = root_idx - i_st;
+        TreeNode* left_tree = constructBinaryTree(inorder, i_st, i_st+left_size-1, preorder, p_st+1, p_st+left_size);
+        TreeNode* right_tree = constructBinaryTree(inorder, i_st+left_size+1, i_end, preorder, p_st+left_size+1, p_end);
+        root->left = left_tree;
+        root->right = right_tree;
+        return root;
+    }
+    
+    int getRootIndex(vector<int>& inorder, int val)
+    {
+        for(int i = 0; i < inorder.size(); ++i)
+        {
+            if(inorder[i] == val)
+                return i;
+        }
+        return -1;
+    }
+};
+
+
+Conclusion:
+a medium level algorithm issue, given the inorder traversal and preorder traversal of a tree, required to reconstruct this tree according to
+those two orders.
+As common sense, we know that, the first element of the preorder is the tree's root node, according to this value, we can find the position
+of this value in the inorder travesal array, then the left part of this element is the left subtree, and the right part of this element is the
+right sub tree. Then we do it recursively, and we will get this tree finally.
+Time Complexity: O(n)
+Space Complexity: O(1)
