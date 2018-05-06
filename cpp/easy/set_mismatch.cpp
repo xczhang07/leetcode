3f908ffb5fe09dcea85ed90f29cc9d1ef82c89fd
Xiaochong Zhang <xiaochongzhang@Xiaochongs-MacBook-Pro.local>
2018-05-05 21:30:26 -0700

    add one more solution for set_mismatch issue on leetcode

--- cpp/easy/set_mismatch.cpp
+++ cpp/easy/set_mismatch.cpp
@@ -23,7 +23,32 @@ public:
 };
 
 
+
+
 Conclusion:
 Easy algorithm issue.
 Time Complexity: O(n)
 Space Complexity: O(n)
+
+
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            while(nums[i] != nums[nums[i]-1])
+                swap(nums[i], nums[nums[i]-1]);
+        }
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if(nums[i] != i+1)
+                return {nums[i], i+1};
+        }
+    }
+};
+
+Conclusion:
+space O(1) solution: map the number from 1 to n to the array for the index i+1, when we found out nums[i] != i+1
+then, the result is: [nums[i], i+1]
+Time complexity: O(n)
+Space Complexity: O(1) 
