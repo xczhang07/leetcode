a08b2d4d8e9db095872b1ba65671472f1def98c5
Xiaochong Zhang <xiaochongzhang@Xiaochongs-MacBook-Pro.local>
2018-05-24 13:21:53 -0700

    resolve the rotate array issue on leetcode

--- cpp/easy/rotate_array.cpp
+++ cpp/easy/rotate_array.cpp
@@ -32,3 +32,25 @@ This is an easy algorithm issue.
 first of all, we reverse the whole array; then we reverse the first kth elements; after that, reverse the rest n-k elements of the array.
 Time Complexity: O(n)
 Space Complexity: O(1)
+
+class Solution {
+public:
+    
+    void rotate(vector<int>& nums, int k) {
+        if(k == 0 || nums.size() == 0)
+            return;
+        int t = 0;
+        while(t < k)
+        {
+            int temp = nums[nums.size()-1];
+            nums.insert(nums.begin(), temp);
+            nums.pop_back();
+            t++;
+        }
+        return;
+    }
+};
+
+Conclusion: just move the tail element of the array to the front until the operation k times.
+Time Complexity: O(k)
+Space Complexity: O(1)
