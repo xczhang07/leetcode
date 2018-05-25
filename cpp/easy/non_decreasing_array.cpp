481517ff9d56f0d7829c5a93aac7ac0e635c25e5
Xiaochong Zhang <xiaochongzhang@Xiaochongs-MacBook-Pro.local>
2018-05-25 16:29:18 -0700

    resolve the non-decreasing array issue

--- cpp/easy/non_decreasing_array.cpp
+++ cpp/easy/non_decreasing_array.cpp
@@ -28,3 +28,32 @@ Conclusion:
 
 Time Complexity: O(n)
 Space Complexity: O(1)
+
+Second solustion:
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        if(nums.size() == 0 || nums.size() == 1)
+            return true;
+        for(int i = 0; i < nums.size()-1; ++i)
+        {
+            if(nums[i] > nums[i+1])
+            {
+                int temp = nums[i];
+                /* remove nums[i] */
+                nums[i] = nums[i+1];
+                if(is_sorted(nums.begin(), nums.end()))
+                    return true;
+                /* erase nums[i+1] */
+                nums[i] = nums[i+1] = temp;
+                if(is_sorted(nums.begin(), nums.end()))
+                    return true;
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+using c++ is_sorted function to resolve this issue.
