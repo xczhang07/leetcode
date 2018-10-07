591947403fa9cf94231d8b49ad9aa7a31130b1ab
xc-zhang <xczhang07@gmail.com>
2018-10-06 22:17:55 -0700

    Create shuffle_an_array.cpp

---
+++ cpp/middle/shuffle_an_array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> origin;
+    vector<int> back;     //back to original
+    Solution(vector<int> nums) {
+        
+        back=nums;
+        origin=nums;
+      
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        
+        return back;
+        
+    }
+    
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        
+        next_permutation(origin.begin(),origin.end());
+        return origin;
+        
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * vector<int> param_1 = obj.reset();
+ * vector<int> param_2 = obj.shuffle();
+ */
+ 
+ /*
+ Conclusion:
+ an ood design algorithm issue, middle level on leetcode, we can directly use c++ "next_permutation" function to resolve it.
+ */
