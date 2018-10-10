933cb29b8dd2b95cc969d98adf8e30b4b1f7086c
xc-zhang <xczhang07@gmail.com>
2018-10-10 00:42:22 -0700

    Update longest_palindromic_subsequence.cpp

--- cpp/middle/longest_palindromic_subsequence.cpp
+++ cpp/middle/longest_palindromic_subsequence.cpp
@@ -30,3 +30,32 @@ else: dp[i][j] = max(dp[i+1][j], dp[i][j-1])
 Time Complexity is: O(n^2)
 Space Complexity is: O(n^2)
 */
+
+
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        /*
+        state transfer function as following:
+        if s[i] == s[j] => dp[j][i] = dp[j+1][i-1] + 2;
+        else dp[j][i] = max(dp[j+1][i], dp[j][i-1])
+        
+        */
+        if(s.size() == 0)
+            return 0;
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n,0));
+        for(int i = 0; i < n; ++i)
+        {
+            dp[i][i] = 1;
+            for(int j = i-1; j >= 0; --j)
+            {
+                if(s[i] == s[j])
+                    dp[j][i] = dp[j+1][i-1]+2;
+                else
+                    dp[j][i] = max(dp[j+1][i], dp[j][i-1]);
+            }
+        }
+        return dp[0][n-1];
+    }
+};
