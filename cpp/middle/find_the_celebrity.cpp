38784bb022e6b5c0de8df12f160421c6034a03ca
xc-zhang <xczhang07@gmail.com>
2018-10-10 01:03:53 -0700

    Create find_the_celebrity.cpp

---
+++ cpp/middle/find_the_celebrity.cpp
@@ -0,0 +1,28 @@
+// Forward declaration of the knows API.
+bool knows(int a, int b);
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        int celebrity = 0;
+        for(int i = 1; i < n; ++i)
+        {
+            if(!knows(i, celebrity))
+                celebrity = i;
+        }
+        for(int i = 0; i < n; ++i)
+        {
+            if(i != celebrity)
+            {
+                if(knows(celebrity, i) || !knows(i, celebrity))
+                    return -1;
+            }
+        }
+        return celebrity;
+    }
+};
+
+
+/*
+Not diffcult, no comments!
+*/
