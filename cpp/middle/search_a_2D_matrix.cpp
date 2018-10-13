f7d10f69b8e3d514f9420227f0b5edf358866a29
xc-zhang <xczhang07@gmail.com>
2018-10-13 14:52:03 -0700

    Update search_a_2D_matrix.cpp

--- cpp/middle/search_a_2D_matrix.cpp
+++ cpp/middle/search_a_2D_matrix.cpp
@@ -55,3 +55,50 @@ public:
         return false;
     }
 };
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.size() == 0 || matrix[0].size() == 0)
+            return false;
+        int r = matrix.size();
+        int c = matrix[0].size();
+        if(target < matrix[0][0] || target > matrix[r-1][c-1])
+            return false;
+        int left = 0;
+        int right = r-1;
+        while(left <= right)
+        {
+            int mid = left + (right - left)/2;
+            if(matrix[mid][0] == target)
+                return true;
+            else if(matrix[mid][0] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        int e = right;
+        left = 0;
+        right = c - 1;
+        while(left <= right)
+        {
+            int mid = left + (right - left)/2;
+            if(matrix[e][mid] == target)
+                return true;
+            else if(matrix[e][mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return false;
+    }
+};
+
+
+/*
+Conclusion:
+run binary search on row and column properly. 
+Time Complexity: O(logM+logN)
+Space Complexity: O(1)
+*/
