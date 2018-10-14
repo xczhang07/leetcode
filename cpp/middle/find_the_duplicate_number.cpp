91faa9b257721fa1beabf08ef3558adbbfa9e96a
xc-zhang <xczhang07@gmail.com>
2018-10-13 17:21:00 -0700

    Update find_the_duplicate_number.cpp

--- cpp/middle/find_the_duplicate_number.cpp
+++ cpp/middle/find_the_duplicate_number.cpp
@@ -22,3 +22,34 @@ public:
 
 Time Complexity: O(n)
 Space Complexity: O(1)
+
+    
+    class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        /*
+        Time Complexity is: O(nlogn)
+        */
+        int l = 1;
+        int h = nums.size()-1;
+        int cnt = 0;
+        while(l <= h)
+        {
+            cnt = 0;
+            int mid = l + (h-l)/2;
+            for(auto n : nums)
+            {
+                if (n <= mid)
+                    cnt++;
+            }
+            if(cnt <= mid)
+                l = mid + 1;
+            else
+                h = mid-1;
+        }
+        return l;
+    }
+};
+
+Time Complexity: O(nlogn)
+Space Complexity: O(1)
