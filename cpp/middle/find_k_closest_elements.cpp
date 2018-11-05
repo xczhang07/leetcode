38f2a2149f3f72ece8f7e184a3500109a9e61e6d
xc-zhang <xczhang07@gmail.com>
2018-11-04 20:41:59 -0800

    Create find_k_closest_elements.cpp

---
+++ cpp/middle/find_k_closest_elements.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int left = 0;
+        int right = arr.size()-1-k;
+        while(left <= right)
+        {
+            int mid = left + (right - left)/2;
+            if(x - arr[mid] > arr[mid+k] - x)
+                left = mid+1;
+            else
+                right = mid-1;
+        }
+        return vector<int>(arr.begin()+left, arr.begin()+left+k);
+    }
+};
+
+
+/*
+http://www.cnblogs.com/grandyang/p/7519466.html
+
+Time Complexity is: O(n)
+Space Complexity is: O(1)
+*/
