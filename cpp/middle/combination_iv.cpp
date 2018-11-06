f34d82569bf24a0005e84b02ba4abdaa20754ac6
xc-zhang <xczhang07@gmail.com>
2018-11-05 16:38:11 -0800

    Create combination_iv.cpp

---
+++ cpp/middle/combination_iv.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1, 0);
+        dp[0] = 1;
+        for(int i = 1; i <= target; ++i)
+        {
+            for(int j = 0; j < nums.size(); ++j)
+            {
+                if(i >= nums[j])
+                    dp[i] += dp[i-nums[j]];
+            }
+        }
+        return dp.back();
+    }
+};
+
+
+/*
+a middle level algorithm issue.
+Time Complexity is: O(t*N)
+Space Complexity is: O(t)
+*/
