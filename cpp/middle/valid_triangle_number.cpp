765e1c070fc3624b7cf2f73706950a166d28395d
xc-zhang <xczhang07@gmail.com>
2018-11-07 21:35:28 -0800

    Update valid_triangle_number.cpp

--- cpp/middle/valid_triangle_number.cpp
+++ cpp/middle/valid_triangle_number.cpp
@@ -28,5 +28,37 @@ Conclusion：
 这道题的题意就是给定一个数组，写一个算法，计算出这个数组里面的数字，能组成多少个三角形。
 brute force，简单暴力.
 
-Time Complexity: O(n^2)
+Time Complexity: O(n^3)
 Space Complexity: O(1)
+   
+Solution 2
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        if(nums.size() < 3)
+            return 0;
+        int ret = 0;
+        int n = nums.size()-1;
+        sort(nums.begin(), nums.end());
+        for(int i = n; i >= 2; --i)
+        {
+            int left = 0;
+            int right = i-1;
+            while(left < right)
+            {
+                int sum = nums[left]+nums[right];
+                if(sum > nums[i])
+                {
+                    ret += (right-left);
+                    --right;
+                }
+                else
+                    ++left;
+            }
+        }
+        return ret;
+    }
+};
+
+Time Complexity is: O(n^2)
+Space Complexity is: O(1)
