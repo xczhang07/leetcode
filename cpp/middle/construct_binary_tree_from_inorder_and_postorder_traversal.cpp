474f28ade29d312d660dce4fc61a2f71615e6774
xc-zhang <xczhang07@gmail.com>
2018-11-08 22:30:37 -0800

    Update construct_binary_tree_from_inorder_and_postorder_traversal.cpp

--- cpp/middle/construct_binary_tree_from_inorder_and_postorder_traversal.cpp
+++ cpp/middle/construct_binary_tree_from_inorder_and_postorder_traversal.cpp
@@ -49,3 +49,48 @@ Conclustion:
 
 Time Complexity: O(n)
 Space Complexity: O(n)
+    
+ Solution 2: an easy solution. 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        if(inorder.size() != postorder.size())
+            return NULL;
+        else if(inorder.size() == 0)
+            return NULL;
+        int root = postorder.size()-1;
+        return helper(postorder, root, inorder, 0, inorder.size()-1);
+    }
+    
+    
+    TreeNode* helper(vector<int>& post, int& root, vector<int>& inorder, int in_st, int in_end)
+    {
+        if(root == post.size() || in_st > in_end)
+            return NULL;
+        TreeNode* rt = new TreeNode(post[root]);
+        int idx = findIndex(inorder, post[root]);
+        --root;
+        rt->right = helper(post, root, inorder, idx+1, in_end);
+        rt->left = helper(post, root, inorder, in_st, idx-1);
+        return rt;
+    }
+    
+    int findIndex(vector<int>& inorder, int val)
+    {
+        for(int i=0; i < inorder.size(); ++i)
+        {
+            if(inorder[i] == val)
+                return i;
+        }
+        return -1;
+    }
+};
