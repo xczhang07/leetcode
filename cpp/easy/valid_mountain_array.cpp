a6798b4ae283bea7bb2a42a871521476142b258f
xc-zhang <xczhang07@gmail.com>
2018-11-20 22:36:40 -0800

    Create valid_mountain_array.cpp

---
+++ cpp/easy/valid_mountain_array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& A) {
+        if(A.size() <= 2)
+            return false;
+        int i = 0;
+        while( i < A.size()-1 && A[i] < A[i+1])
+            ++i;
+        if(i == 0 || i == A.size()-1)
+            return false;
+        while(i < A.size()-1 && A[i] > A[i+1])
+            ++i;
+        return i == A.size()-1;
+    }
+};
