62ae0a40a43f766538e2c8026a9c7ed1619b255c
xc-zhang <xczhang07@gmail.com>
2018-11-20 18:19:03 -0800

    Update symmetric_tree.cpp

--- cpp/easy/symmetric_tree.cpp
+++ cpp/easy/symmetric_tree.cpp
@@ -34,3 +34,60 @@ we can use recursive to travel the entire tree, then we can get the result.
 
 Time Complexity: according to travel the entire tree, the time complexity is: O(n)
 Space Complexity: according to the recursive call, the worst case is the call stack is: O(n)
+
+    
+    /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(!root)
+            return true;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int n = q.size();
+            vector<string> level;
+            for(int i = 0; i < n; ++i)
+            {
+                TreeNode* top = q.front();
+                q.pop();
+                if(top == NULL)
+                {
+                    level.push_back("#");
+                    continue;
+                }
+                else
+                    level.push_back(to_string(top->val));
+                if(top->left)
+                    q.push(top->left);
+                else
+                    q.push(NULL);
+                if(top->right)
+                    q.push(top->right);
+                else
+                    q.push(NULL);
+            }
+            int i = 0;
+            int j = level.size()-1;
+            while(i < j)
+            {
+                if(level[i] != level[j])
+                    return false;
+                ++i;
+                --j;
+            }
+        }
+        return true;
+    }
+    
+   
+};
