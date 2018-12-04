8df0ce8cf95a618984dc24a92845d4c2cfbe5609
xc-zhang <xczhang07@gmail.com>
2018-12-03 18:08:56 -0800

    Update insert_interval.cpp

--- cpp/hard/insert_interval.cpp
+++ cpp/hard/insert_interval.cpp
@@ -36,3 +36,42 @@ public:
 
 
 /*a hard level algorithm issue on leetcode, using O(n) time complexity can resolve it, not too diffcult*/
+
+/* 2nd solution */
+
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
+        if(intervals.size() == 0)
+            return {newInterval};
+        vector<Interval>::iterator it = intervals.begin();
+        while(it != intervals.end())
+        {
+            if(newInterval.end < it->start)
+            {
+                intervals.insert(it, newInterval);
+                return intervals;
+            }
+            else if(it->end < newInterval.start)
+                it++;
+            else
+            {
+                newInterval.start = min(newInterval.start, it->start);
+                newInterval.end = max(newInterval.end, it->end);
+                intervals.erase(it);
+            }
+        }
+        intervals.insert(intervals.end(), newInterval);
+        return intervals;
+    }
+};
+
