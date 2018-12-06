cb09f16c31a9e7e6ed9f372a7a5ef589e26ae058
xc-zhang <xczhang07@gmail.com>
2018-12-06 15:09:21 -0800

    Create cheapest_flights_within_k_stops.cpp

---
+++ cpp/middle/cheapest_flights_within_k_stops.cpp
@@ -0,0 +1,43 @@
+DFS version
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+        unordered_map<int, vector<pair<int,int>>> g;
+        buildGraph(flights, g);
+        int min_money = INT_MAX;
+        int total_money = 0;
+        vector<bool> visited(n, false);
+        visited[src] = true;
+        dfs(g, src, dst, K+1, min_money, total_money, visited);
+        return min_money == INT_MAX?-1:min_money;
+    }
+    
+    void buildGraph(vector<vector<int>>& flights, unordered_map<int,vector<pair<int,int>>>& g)
+    {
+        for(int i = 0; i < flights.size(); ++i)
+            g[flights[i][0]].push_back(make_pair(flights[i][1], flights[i][2]));
+    }
+    
+    void dfs(unordered_map<int,vector<pair<int,int>>>& g, int src, int dst, int k, int& min_money, int total_money, vector<bool>& visited)
+    {
+        if(k < 0)
+            return;
+        if(src == dst)
+        {
+            if(k >= 0)
+            {
+                min_money = min(min_money, total_money);
+                return;
+            }
+        }
+        
+        for(int i = 0; i < g[src].size(); ++i)
+        {
+            if(visited[g[src][i].first] || total_money + g[src][i].second > min_money)
+                continue;
+            visited[g[src][i].first] = true;
+            dfs(g, g[src][i].first, dst, k-1, min_money, total_money+g[src][i].second, visited);
+            visited[g[src][i].first] = false;
+        }
+    }
+};
