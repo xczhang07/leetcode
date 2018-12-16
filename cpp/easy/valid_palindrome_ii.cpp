2ce3f19ca414682a4d6b81dc0b2ca1180e81d1a8
xc-zhang <xczhang07@gmail.com>
2018-12-16 15:54:09 -0800

    Update valid_palindrome_ii.cpp

--- cpp/easy/valid_palindrome_ii.cpp
+++ cpp/easy/valid_palindrome_ii.cpp
@@ -45,3 +45,35 @@ Conclusion: an easy algorithm issue, two pointer.
 
 Time Complexity: O(n)
 Space Complexity: O(1)
+
+
+Solution 2, recursive
+
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int l = 0;
+        int r = s.size()-1;
+        int count = 0;
+        return helper(s, l, r, count);
+    }
+    
+    bool helper(string& s, int lo, int hi, int count)
+    {
+        if(count > 1) return false;
+        while(lo < hi)
+        {
+            if(s[lo] == s[hi])
+            {
+                ++lo;
+                --hi;
+            }
+            else
+            {
+                bool ret = helper(s, lo+1, hi, count+1) || helper(s, lo, hi-1, count+1);
+                return ret;
+            }
+        }
+        return true;
+    }
+};
