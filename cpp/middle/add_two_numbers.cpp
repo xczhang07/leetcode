eb950db993a40036c8c6b7d16c86677bb585623f
xc-zhang <xczhang07@gmail.com>
2018-12-16 10:34:10 -0800

    Update add_two_numbers.cpp

--- cpp/middle/add_two_numbers.cpp
+++ cpp/middle/add_two_numbers.cpp
@@ -62,3 +62,38 @@ public:
 Conclusion:
 
 easy issue, straight forward, time complexity is: O(m+n)
+    
+    
+version 2:
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        if(!l1) return l2;
+        if(!l2) return l1;
+        int carry = 0, sum = 0;
+        ListNode* newList = new ListNode(0);
+        ListNode* t = newList;
+        while(l1 || l2 || carry)
+        {
+            sum = (l1?l1->val:0) + (l2?l2->val:0) + carry;
+            carry = sum/10;
+            ListNode* node = new ListNode(sum%10);
+            t->next = node;
+            t = t->next;
+            l1 = l1 ? l1->next:l1;
+            l2 = l2 ? l2->next:l2;
+        }
+        return newList->next;
+    }
+    
+    
+};
