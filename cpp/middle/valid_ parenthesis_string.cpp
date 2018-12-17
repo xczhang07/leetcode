f1ac2f1927310f07572b02ffb978ff1c9f5e909b
xc-zhang <xczhang07@gmail.com>
2018-12-16 17:55:30 -0800

    Create valid_ parenthesis_string.cpp

---
+++ cpp/middle/valid_ parenthesis_string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        if(s.size() == 0) 
+            return true;
+        int left = 0;
+        for(int i = 0; i < s.size(); ++i)
+        {
+            if(s[i] == '*' || s[i] == '(')
+                ++left;
+            else
+            {
+                --left;
+                if(left < 0) return false;
+            }
+        }
+        if(left == 0) return true;
+        int right = 0;
+        for(int i = s.size()-1; i >= 0; --i)
+        {
+            if(s[i] == '*' || s[i] == ')')
+                ++right;
+            else
+            {
+                --right;
+                if(right < 0) return false;
+            }
+        }
+        return true;
+    }
+};
