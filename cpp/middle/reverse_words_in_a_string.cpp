571142bfb658bd9a3455de6eeb76fb57e0f68435
xc-zhang <xczhang07@gmail.com>
2018-12-25 00:29:06 -0800

    Update reverse_words_in_a_string.cpp

--- cpp/middle/reverse_words_in_a_string.cpp
+++ cpp/middle/reverse_words_in_a_string.cpp
@@ -68,3 +68,57 @@ a middle level algorithm issue, not too diffcult, just operation complicated,
 Time Complexity is: O(n)
 Space Complexity is: O(1)
 */
+
+
+Solution 2, much simpler and shorter of the code
+
+class Solution {
+public:
+    void reverseWords(string &s) {
+        int t = 0, l = 0, r = 0;
+        int len = s.size();
+        bool wordCount = 0;
+        
+        while(true)
+        {
+            while(t < len && s[t] == ' ')
+                ++t;
+            if(t == len)
+                break;
+            if(wordCount)
+            {
+                s[r] = ' ';
+                ++r;
+            }
+            l = r;
+            while(t < len && s[t] != ' ')
+            {
+                s[r] = s[t];
+                ++t;
+                ++r;
+            }
+            reverseWord(s, l, r-1);
+            if(!wordCount) wordCount++;
+        }
+        s.resize(r);
+        reverseWord(s, 0, r-1);
+        return;
+        
+    }
+    
+    
+    void reverseWord(string& s, int l, int r)
+    {
+        while(l < r)
+        {
+            char tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            ++l;
+            --r;
+        }
+        return;
+    }
+    
+   
+};
