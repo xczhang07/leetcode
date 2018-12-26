7d9d4f53a4b9243423d7d646c20ee92aebd847a5
xc-zhang <xczhang07@gmail.com>
2018-12-25 18:12:51 -0800

    Update number_of_islands.cpp

--- cpp/middle/number_of_islands.cpp
+++ cpp/middle/number_of_islands.cpp
@@ -40,3 +40,50 @@ a middle level algorithm issue, caculate the number of islands in an input matri
 Time Complexity: O(n^2)
 Space Complexity: O(1)
 */
+
+Solution 2: BFS version
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int ret = 0;
+        if(grid.size() == 0 || grid[0].size() == 0)
+            return ret;
+        queue<pair<int,int>> q;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<pair<int,int>> dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        for(int i = 0; i < m; ++i)
+        {
+            for(int j = 0; j < n; ++j)
+            {
+                if(grid[i][j] == '1')
+                {
+                    ++ret;
+                    q.push(make_pair(i,j));
+                    grid[i][j] = '0';
+                    while(!q.empty())
+                    {
+                        auto p = q.front();
+                        q.pop();
+                        //grid[p.first][p.second] = '0'; cause the memory exceed limit
+                        for(auto dir : dirs)
+                        {
+                            int x = p.first+dir.first;
+                            int y = p.second+dir.second;
+                            if(x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != '1')
+                                continue;
+                            else
+                            {
+                                q.push(make_pair(x,y));
+                                grid[x][y] = '0';
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+    
+};
