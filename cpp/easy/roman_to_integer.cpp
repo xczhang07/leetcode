35f13bc8244ec95d1e4b473206c084a19506ba2f
xc-zhang <xczhang07@gmail.com>
2019-01-05 00:13:19 -0800

    Update roman_to_integer.cpp

--- cpp/easy/roman_to_integer.cpp
+++ cpp/easy/roman_to_integer.cpp
@@ -29,3 +29,40 @@ public:
 
 Conclusion:
 an easy algorithm issue. No comments.
+
+    Solution II:
+    
+    class Solution {
+public:
+    int romanToInt(char c)
+    {
+        if(c == 'I')
+            return 1;
+        else if(c == 'V')
+            return 5;
+        else if(c == 'X')
+            return 10;
+        else if(c == 'L')
+            return 50;
+        else if(c == 'C')
+            return 100;
+        else if(c == 'D')
+            return 500;
+        else if(c == 'M')
+            return 1000;
+        else
+            return 0;
+    }
+    
+    int romanToInt(string s) {
+        int num = 0;
+        for(int i = 0; i < s.size(); ++i)
+        {
+            if(i < s.size()-1 && romanToInt(s[i]) < romanToInt(s[i+1]))
+                num -= romanToInt(s[i]);
+            else
+                num += romanToInt(s[i]);
+        }
+        return num;
+    }
+};
