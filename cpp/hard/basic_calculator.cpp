43facb1dd2e56bd3b3fe0bf727e85e4973add827
xc-zhang <xczhang07@gmail.com>
2019-01-14 22:16:50 -0800

    Update basic_calculator.cpp

--- cpp/hard/basic_calculator.cpp
+++ cpp/hard/basic_calculator.cpp
@@ -38,3 +38,56 @@ public:
         return res;
     }
 };
+
+Time Complexity is: O(n^2)
+Space Complexity is: O(n)
+    
+Solution 2:
+class Solution {
+public:
+    int calculate(string s) {
+        if(s.size() == 0)
+            return 0;
+        int i = 0;
+        return parseExp(s, i);
+    }
+    
+    int parseExp(string& s, int& i)
+    {
+        char op = '+';
+        int ret = 0;
+        int num = 0;
+        while(i < s.size() && op != ')')
+        {
+            num = 0;
+            if(s[i] == ' ')
+            {
+                ++i;
+                continue;
+            }
+            else if(isdigit(s[i]))
+            {
+                while(i < s.size() && isdigit(s[i]))
+                {
+                    num = num * 10 + (s[i]-'0');
+                    ++i;
+                }
+            }
+            else if(s[i] == '(')
+            {
+                ++i;
+                num = parseExp(s, i);
+            }
+            if(op == '+')
+                ret += num;
+            else if(op == '-')
+                ret -= num;
+            op = s[i];
+            ++i;
+        }
+        return ret;
+    }
+};
+
+Time Complexity is: O(n)
+Space Complexity is: O(n)
