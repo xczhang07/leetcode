7de565140b94136f0479f772a1bf817fdce61824
xc-zhang <xczhang07@gmail.com>
2019-01-16 15:29:00 -0800

    Update meeting_rooms_ii.cpp

--- cpp/middle/meeting_rooms_ii.cpp
+++ cpp/middle/meeting_rooms_ii.cpp
@@ -66,3 +66,43 @@ public:
         return ret;
     }
 };
+
+
+
+
+Solution3: O(m+n) time complexity
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        if(intervals.size() == 0)
+            return 0;
+        int max_time = 0;
+        for(int i = 0; i < intervals.size(); ++i)
+            max_time = max(max_time, intervals[i].end);
+        vector<int> times(max_time+1, 0);
+        int rooms = 1;
+        int count = 0;
+        for(int i = 0; i < intervals.size(); ++i)
+        {
+            times[intervals[i].start]++;
+            times[intervals[i].end]--;
+        }
+        for(int i = 0; i <= max_time; ++i)
+        {
+            count += times[i];
+            rooms = max(rooms, count);
+        }
+        return rooms;
+    }
+};
