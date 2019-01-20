d04503bdecdd950b37094681877f4d4a26d58771
xc-zhang <xczhang07@gmail.com>
2019-01-19 17:43:53 -0800

    Update zigzag_iterator.cpp

--- cpp/middle/zigzag_iterator.cpp
+++ cpp/middle/zigzag_iterator.cpp
@@ -35,3 +35,38 @@ private:
  Time Complexity is: O(l1+l2)
  Space Complexity is: O(1)
  */
+
+
+K array case:
+class ZigzagIteratorK {
+public:
+    //TC: O(K) to push all iterators into the queue
+    //SC: O(1), the queue takes only reference to the input variable v 
+    //and does not create extra data structures
+    ZigzagIteratorK(vector<vector<int>>& v) {
+        for (int i = 0; i < v.size(); i++) {
+            if (v[i].size() > 0)
+                m_q.emplace(v[i].begin(), v[i].end());
+        }   
+    }
+    
+    int next() {
+        //TC: O(1)
+        //SC: O(1)
+        vector<int>::iterator it1 = m_q.front().first;
+        vector<int>::iterator it2 = m_q.front().second;
+        m_q.pop();
+        if (it1 + 1 != it2)
+            m_q.emplace(it1 + 1, it2);
+        return *it1;
+    }
+    
+    bool hasNext() {
+        //TC: O(1)
+        //SC: O(1)
+        return !m_q.empty();
+    }
+    
+private:
+    queue<pair<vector<int>::iterator, vector<int>::iterator>> m_q;
+};
