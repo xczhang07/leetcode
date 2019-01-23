2be3b7441bbcb63bea52c79990b429c3b517b875
xc-zhang <xczhang07@gmail.com>
2019-01-22 22:46:44 -0800

    Update read_n_charactors_from_read4_ii.cpp

--- cpp/hard/read_n_charactors_from_read4_ii.cpp
+++ cpp/hard/read_n_charactors_from_read4_ii.cpp
@@ -35,3 +35,41 @@ public:
 private:
     queue<char> buffer;
 };
+
+
+
+Solution 2
+// Forward declaration of the read4 API.
+int read4(char *buf);
+
+class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    int read(char *buf, int n) {
+       if(n == 0)
+           return 0;
+        for(int i = 0; i < n; ++i)
+        {
+            if(read_bytes == write_bytes)
+            {
+                write_bytes = read4(buff);
+                read_bytes = 0;
+                if(write_bytes == 0)
+                    return i;
+            }
+            buf[i] = buff[read_bytes];
+            read_bytes++;
+        }
+        return n;
+    }
+    
+private:
+    int read_bytes = 0;
+    int write_bytes = 0;
+    char buff[4];
+    
+};
