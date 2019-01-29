8fc8d358560ed5a5ccadc311d9f3f10101eb044c
xc-zhang <xczhang07@gmail.com>
2019-01-28 17:58:54 -0800

    Update sort_charactors_by_frequency.cpp

--- cpp/middle/sort_charactors_by_frequency.cpp
+++ cpp/middle/sort_charactors_by_frequency.cpp
@@ -21,6 +21,38 @@ public:
 
 /*
 Time Complexity is: O(nlogn)
-Space Complexity is: O(n)
+Space Complexity is: O(1)
 
 */
+
+Solution# 2:
+bucket sort version solution
+
+Time Complexity is: O(n)
+Space Complexity is: O(n)
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        if(s.size() <= 1)
+            return s;
+        vector<string> bucket(s.size()+1, "");
+        unordered_map<char, int> freq;
+        string ret;
+        for(int i = 0; i < s.size(); ++i)
+            freq[s[i]]++;
+        for(auto f : freq)
+        {
+            int idx = f.second;
+            int val = f.first;
+            bucket[idx].append(idx, val);
+        }
+        
+        for(int i = bucket.size()-1; i >= 0; --i)
+        {
+            if(bucket[i].size() != 0)
+                ret += bucket[i];
+        }
+        return ret;
+    }
+};
