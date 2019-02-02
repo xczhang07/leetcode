179d285dfab6ce8a35b7cbb740539d8ef41cbb8a
xc-zhang <xczhang07@gmail.com>
2019-02-01 19:38:35 -0800

    Update construct_binary_tree_from_string.cpp

--- cpp/middle/construct_binary_tree_from_string.cpp
+++ cpp/middle/construct_binary_tree_from_string.cpp
@@ -51,3 +51,53 @@ public:
 Time Complexity is: O(n)
 Space Complexity is: O(n)
 */
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+TreeNode* builder(string s, int& idx)
+{
+    int num = 0;
+    int sign = 1;
+    TreeNode* root = new TreeNode(0);
+    for(idx; idx < s.size(); ++idx)
+    {
+        if(s[idx] == '-')
+            sign = -1;
+        while(idx < s.size() && isdigit(s[idx]))
+        {
+            num = num * 10 + (s[idx]-'0');
+            ++idx;
+        }
+        root->val = num * sign;
+        if(s[idx] == '(')
+        {
+            TreeNode* child = builder(s, ++idx);
+            if(root->left == NULL)
+                root->left = child;
+            else
+                root->right = child;
+        }
+        else if(s[idx] == ')')
+            return root;
+    }
+    return root;
+}
+    
+    
+TreeNode* str2tree(string s) {
+    if (s.empty()) return NULL;
+    int i = 0;
+    return builder(s, i);
+}
+
+};
