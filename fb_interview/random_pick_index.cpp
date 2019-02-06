c29b97923e3aaff9fec7182e9436d366c734466e
xc-zhang <xczhang07@gmail.com>
2019-02-06 00:17:44 -0800

    Create random_pick_index.cpp

---
+++ fb_interview/random_pick_index.cpp
@@ -0,0 +1,50 @@
+Given an array of integers with possible duplicates, randomly output the index of a given target number. You can assume that the given target number must exist in the array.
+
+Note:
+The array size can be very large. Solution that uses too much extra space will not pass the judge.
+
+Example:
+
+int[] nums = new int[] {1,2,3,3,3};
+Solution solution = new Solution(nums);
+
+// pick(3) should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
+solution.pick(3);
+
+// pick(1) should return 0. Since in the array only nums[0] is equal to 1.
+solution.pick(1);
+
+class Solution {
+public:
+    Solution(vector<int> nums) {
+        this->input = nums;
+    }
+    
+    int pick(int target) {
+        int cnt = 0;
+        int ret = -1;
+        
+        for(int i = 0; i < input.size(); ++i)
+        {
+            if(target != input[i])
+                continue;
+            else
+            {
+                cnt++;
+                if(rand()%cnt == 0)
+                    ret = i;
+            }
+        }
+        
+        return ret;
+    }
+    
+private:
+    vector<int> input;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
