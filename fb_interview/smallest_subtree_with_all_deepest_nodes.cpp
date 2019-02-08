e0203f4b0d16685d6ff6e72b9010ef237a7ae968
xc-zhang <xczhang07@gmail.com>
2019-02-08 00:57:34 -0800

    Create smallest_subtree_with_all_deepest_nodes.cpp

---
+++ fb_interview/smallest_subtree_with_all_deepest_nodes.cpp
@@ -0,0 +1,60 @@
+Given a binary tree rooted at root, the depth of each node is the shortest distance to the root.
+
+A node is deepest if it has the largest depth possible among any node in the entire tree.
+
+The subtree of a node is that node, plus the set of all descendants of that node.
+
+Return the node with the largest depth such that it contains all the deepest nodes in its subtree.
+
+ 
+
+Example 1:
+
+Input: [3,5,1,6,2,0,8,null,null,7,4]
+Output: [2,7,4]
+Explanation:
+
+
+
+We return the node with value 2, colored in yellow in the diagram.
+The nodes colored in blue are the deepest nodes of the tree.
+The input "[3, 5, 1, 6, 2, 0, 8, null, null, 7, 4]" is a serialization of the given tree.
+The output "[2, 7, 4]" is a serialization of the subtree rooted at the node with value 2.
+Both the input and output have TreeNode type.
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    pair<int, TreeNode*> find(TreeNode* root)
+    {
+        if(!root)
+            return make_pair(0, root);
+        auto left = find(root->left);
+        auto right = find(root->right);
+        if(left.first == right.first)
+            return make_pair(left.first+1, root);
+        else if(left.first > right.first)
+            return make_pair(left.first+1, left.second);
+        else
+            return make_pair(right.first+1, right.second);
+    }
+    
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        if(!root)
+            return NULL;
+        return find(root).second;
+    }
+    
+    
+};
