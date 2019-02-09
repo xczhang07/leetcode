787fa3d3443bcaf19510120db5c885c57536cef5
xc-zhang <xczhang07@gmail.com>
2019-02-08 17:21:16 -0800

    Update flatten_binary_tree_to_linked_list.cpp

--- cpp/middle/flatten_binary_tree_to_linked_list.cpp
+++ cpp/middle/flatten_binary_tree_to_linked_list.cpp
@@ -30,3 +30,44 @@ convert a binary tree to linked list.
 Time Complexity: O(n)
 Space Complexity: O(1)
 */
+
+
+Solution#2 iteration version
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(!root)
+            return;
+        stack<TreeNode*> st;
+        while(root)
+        {
+            if(root->right)
+                st.push(root->right);
+            if(root->left)
+            {
+                root->right = root->left;
+                root->left = NULL;
+            }
+            else if(!st.empty())
+            {
+                root->right = st.top();
+                st.pop();
+            }
+            root = root->right;
+        }
+        return;
+    }
+};
+
+Time Complexity is: O(n)
+Space Complexity is: O(n)
