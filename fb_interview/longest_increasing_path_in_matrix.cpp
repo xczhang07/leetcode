6cf2062f22e5831b41c117d41843ef7dfc3e6063
xc-zhang <xczhang07@gmail.com>
2019-02-16 20:24:47 -0800

    Update longest_increasing_path_in_matrix.cpp

--- fb_interview/longest_increasing_path_in_matrix.cpp
+++ fb_interview/longest_increasing_path_in_matrix.cpp
@@ -1,6 +1,6 @@
 Given an integer matrix, find the length of the longest increasing path.
-
-From each cell, you can either move to four directions: left, right, up or down. You may NOT move diagonally or move outside of the boundary (i.e. wrap-around is not allowed).
+From each cell, you can either move to four directions: 
+left, right, up or down. You may NOT move diagonally or move outside of the boundary (i.e. wrap-around is not allowed).
 
 Example 1:
 
