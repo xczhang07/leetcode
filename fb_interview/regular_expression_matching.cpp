058b104273d6e95e37c99699b075b57b8d3cfa9e
xc-zhang <xczhang07@gmail.com>
2019-02-18 16:33:35 -0800

    Create regular_expression_matching.cpp

---
+++ fb_interview/regular_expression_matching.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if(s.size() == 0 && p.size() == 0)
+            return true;
+        int m = s.size(), n = p.size();
+        
+        vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
+        
+        dp[0][0] = true;
+        for(int i = 2; i <= n; ++i)
+        {
+            if(p[i-1] == '*')
+                dp[0][i] = dp[0][i-2];
+        }
+        
+        for(int i = 1; i <= m; ++i)
+        {
+            for(int j = 1; j <= n; ++j)
+            {
+                if(p[j-1] == s[i-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] == '.')
+                    dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] == '*')
+                {
+                    if(p[j-2] != s[i-1] && p[j-2] != '.')
+                        dp[i][j] = dp[i][j-2];
+                    else
+                        dp[i][j] = dp[i][j-1]||dp[i][j-2]||dp[i-1][j];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    
+};
