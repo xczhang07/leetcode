efbac082f0c7584163aa6efeb2b687d4a39494ad
xc-zhang <xczhang07@gmail.com>
2019-02-24 21:42:54 -0800

    Update displayPages.cpp

--- airbnb_interview.cpp/displayPages.cpp
+++ airbnb_interview.cpp/displayPages.cpp
@@ -46,3 +46,35 @@ vector<string> displayPages(vector<string> input, int num)
     
     return pages;
 }
+
+
+int main()
+{
+     vector<string> input = {
+        "1,28,310.6,SF",
+        "4,5,204.1,SF",
+        "20,7,203.2,Oakland",
+        "6,8,202.2,SF",
+        "6,10,199.1,SF",
+        "1,16,190.4,SF",
+        "6,29,185.2,SF",
+        "7,20,180.1,SF",
+        "6,21,162.1,SF",
+        "2,18,161.2,SF",
+        "2,30,149.1,SF",
+        "3,76,146.2,SF",
+        "2,14,141.1,San Jose"
+    };
+    
+    vector<string> pages = displayPages(input, 4);
+    
+    for(int i = 0; i < pages.size(); ++i)
+    {
+        if(pages[i] != " ")
+            cout<<pages[i]<<endl;
+        else
+            cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
