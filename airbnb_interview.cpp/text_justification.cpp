42695b519e98d449fed0c09481c10aa536dcefc1
xc-zhang <xczhang07@gmail.com>
2019-02-25 22:47:21 -0800

    Create text_justification.cpp

---
+++ airbnb_interview.cpp/text_justification.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        vector<string> ret;
+        if(words.size() == 0)
+            return ret;
+        int i = 0;
+        while(i < words.size())
+        {
+            int j = i, len = 0;
+            while(j < words.size() && len + words[j].size() + j-i <= maxWidth)
+            {
+                len += words[j].size();
+                ++j;
+            }
+            int spaces = maxWidth-len;
+            string line;
+            for(int k = i; k < j; ++k)
+            {
+                line += words[k];
+                int tmp = 0;
+                if(spaces > 0)
+                {
+                    if(j == words.size())
+                    {
+                        if(j-k == 1)
+                            tmp = spaces;
+                        else
+                            tmp = 1;
+                    }
+                    else
+                    {
+                        if(j - k > 1)
+                        {
+                            if(spaces%(j-k-1) == 0)
+                                tmp = spaces/(j-k-1);
+                            else
+                                tmp = spaces/(j-k-1)+1;
+                        }
+                        else
+                            tmp = spaces;
+                    }
+                }
+                spaces -= tmp;
+                line.append(tmp, ' ');
+            }
+            ret.push_back(line);
+            i = j;
+        }
+        return ret;
+    }
+};
