77c3800f2043e43eefa80431304ca2e0735debce
xc-zhang <xczhang07@gmail.com>
2019-03-10 15:07:07 -0700

    Update merge_k_sorted_lists.cpp

--- google_interview/merge_k_sorted_lists.cpp
+++ google_interview/merge_k_sorted_lists.cpp
@@ -64,3 +64,54 @@ public:
     }
     
 };
+
+priority queue solution:
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+bool compare(ListNode* l1, ListNode* l2)
+{
+    return l1->val > l2->val;
+}
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size() == 0)
+            return NULL;
+        else if(lists.size() == 1)
+            return lists[0];
+        priority_queue<ListNode*, vector<ListNode*>, function<bool(ListNode*, ListNode*)>> pq(compare);
+        for(int i = 0; i < lists.size(); ++i)
+        {
+            if(lists[i])
+                pq.push(lists[i]);
+        }
+        if(pq.size() == 0)
+            return NULL;
+        ListNode* ret = pq.top();
+        pq.pop();
+        ListNode* tail = ret;
+        if(tail->next)
+            pq.push(tail->next);
+        while(!pq.empty())
+        {
+            ListNode* top = pq.top();
+            pq.pop();
+            tail->next = top;
+            tail = tail->next;
+            if(tail->next)
+                pq.push(tail->next);
+        }
+        return ret;
+        
+    }
+    
+};
