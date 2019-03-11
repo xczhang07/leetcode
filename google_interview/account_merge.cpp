7c9d2b9f54407ca58ba43335a6b91311d9930817
xc-zhang <xczhang07@gmail.com>
2019-03-10 17:08:52 -0700

    Update account_merge.cpp

--- google_interview/account_merge.cpp
+++ google_interview/account_merge.cpp
@@ -56,3 +56,64 @@ public:
     
     
 };
+
+Solution#2 Union-Find
+
+class Solution {
+public:
+    
+    string find(unordered_map<string, string>& parent, string email)
+    {
+        while(email != parent[email])
+            email = parent[email];
+        return email;
+    }
+    
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        unordered_map<string, string> parents;
+        unordered_map<string, string> owner;
+        unordered_map<string, set<string>> unions;
+        vector<vector<string>> ret;
+        /* construct the parent relationship */
+        for(int i = 0; i < accounts.size(); ++i)
+        {
+            for(int j = 1; j < accounts[i].size(); ++j)
+            {
+                string email = accounts[i][j];
+                string person = accounts[i][0];
+                parents[email] = email;
+                owner[email] = person;
+            }
+        }
+        
+        /* connect the element who has the same parent */
+        for(int i = 0; i < accounts.size(); ++i)
+        {
+            string p1 = find(parents, accounts[i][1]);
+            for(int j = 2; j < accounts[i].size(); ++j)
+            {
+                string p2 = find(parents, accounts[i][j]);
+                parents[p2] = p1;
+            }
+        }
+        
+        /* combine the email who has the same parent */
+        /* set the union for the email address which has the same parent */
+        for(int i = 0; i < accounts.size(); ++i)
+        {
+            for(int j = 1; j < accounts[i].size(); ++j)
+            {
+                string parent = find(parents, accounts[i][j]);
+                unions[parent].insert(accounts[i][j]);
+            }
+        }
+        
+        for(auto e : unions)
+        {
+            vector<string> emails(e.second.begin(), e.second.end());
+            emails.insert(emails.begin(), owner[e.first]);
+            ret.push_back(emails);
+        }
+        return ret;
+    }
+};
