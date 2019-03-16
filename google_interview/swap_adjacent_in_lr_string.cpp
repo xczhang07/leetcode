c6b261288e0396ee1c057fa0aac0520b17ae3f94
xc-zhang <xczhang07@gmail.com>
2019-03-16 16:29:02 -0700

    Update swap_adjacent_in_lr_string.cpp

--- google_interview/swap_adjacent_in_lr_string.cpp
+++ google_interview/swap_adjacent_in_lr_string.cpp
@@ -1,5 +1,8 @@
 
-In a string composed of 'L', 'R', and 'X' characters, like "RXXLRXRXL", a move consists of either replacing one occurrence of "XL" with "LX", or replacing one occurrence of "RX" with "XR". Given the starting string start and the ending string end, return True if and only if there exists a sequence of moves to transform one string to the other.
+In a string composed of 'L', 'R', and 'X' characters, like "RXXLRXRXL", 
+a move consists of either replacing one occurrence of "XL" with "LX", or replacing one occurrence of "RX" with "XR".
+Given the starting string start and the ending string end, 
+return True if and only if there exists a sequence of moves to transform one string to the other.
 
 Example:
 
