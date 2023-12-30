0728abd1a37310e4a4d54b7bf57fa8cca3ef84fd
xc-zhang <xczhang07@gmail.com>
2023-12-30 00:27:10 -0800

    Update kth_largest_element_in_an_array.cpp

--- cpp/middle/kth_largest_element_in_an_array.cpp
+++ cpp/middle/kth_largest_element_in_an_array.cpp
@@ -21,3 +21,51 @@ we can use priority queue to resolve this issue.
 
 Time Complexity: O(n+klogn), every time, the pop operation of priority_queue is logn, and we need to perform this operation k times.
 Space Complexity: O(n)
+
+Solution 2:
+Using quickselect to resolve this issue, the avg runtime is O(n) , the worst case is O(n^2); the avg runtime is better than the using heap solution.
+class Solution {
+public:
+    int partition(vector<int>& nums, int k, int start, int end){
+        if (start==end){
+            return start;
+        }
+        int pivot = rand()%(end-start+1)+start;
+        swap(nums[pivot], nums[end]);
+        pivot = start;
+        for (int i=start; i<end; i++){
+            if (nums[i]<nums[end]){
+                swap(nums[i], nums[pivot++]);
+            }
+        }
+        swap(nums[pivot], nums[end]);
+        return pivot;
+    }
+
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        int target = n-k;
+        int start = 0;
+        int end = n-1;
+        int pivot=0;
+        while (true){
+            pivot = partition(nums, target, start, end);
+            if (pivot==target){
+                return nums[target];
+            }
+            int tmp = pivot;
+            if (target>pivot){
+                
+                while (tmp<=target-2 && nums[tmp]==nums[tmp+1]){
+                    tmp++;
+                }
+                start = tmp+1;
+            } else{
+                while (tmp>=target+2 && nums[tmp]==nums[tmp-1]){
+                    tmp--;
+                }
+                end = tmp-1;
+            }
+        }
+    }
+};
