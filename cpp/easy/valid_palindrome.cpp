b9415b4f261fb01590c7483f2022cc388ebd0678
xc-zhang <xczhang07@gmail.com>
2024-01-10 14:58:33 -0800

    Update valid_palindrome.cpp

--- cpp/easy/valid_palindrome.cpp
+++ cpp/easy/valid_palindrome.cpp
@@ -31,3 +31,40 @@ Conclusion:
 an easy algorithm issue, create a tmp string as a template dictionary to check whether or not an element is alphanumeric charactors.
 Time Complexity is: O(n)
 Space Complexity is: O(1)
+
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.size() == 0) {
+            return true;
+        }
+        int i = 0, j = s.size()-1;
+        while(i < j) {
+           if(isalnum(s[i]) && isalnum(s[j])) {
+               s[i] = tolower(s[i]);
+               s[j] = tolower(s[j]);
+               if(s[i] == s[j]) {
+                   ++i;
+                   --j;
+               } else {
+                   return false;
+               }
+           } else {
+               if(!isalnum(s[i])) {
+                   ++i;
+                   if(i > j) {
+                       return true;
+                   }
+               }
+               if(!isalnum(s[j])) {
+                   --j;
+                   if(i > j) {
+                       return true;
+                   }
+               }
+           }
+        }
+        return true;
+    }
+};
