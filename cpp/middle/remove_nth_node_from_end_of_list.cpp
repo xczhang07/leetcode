93302757cb308ec499fba1a0449d238b288f5de5
xc-zhang <xczhang07@gmail.com>
2024-01-13 00:32:03 -0800

    Update remove_nth_node_from_end_of_list.cpp

--- cpp/middle/remove_nth_node_from_end_of_list.cpp
+++ cpp/middle/remove_nth_node_from_end_of_list.cpp
@@ -49,3 +49,42 @@ a middle level issue of leetcode, this solution is pretty much straight forward,
 Time Complexity: O(n)
 Space Complexity: O1)
 */
+
+
+# Do it in one pass.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(!head) {
+            return head;
+        }
+        if(n == 0) {
+            return head;
+        }
+        ListNode* fast = head, *slow = head;
+        for(int i = 0; i < n; ++i) {
+            fast = fast -> next;
+        }
+        if(!fast) {
+            return head->next;
+        }
+        while(fast->next) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        slow->next = slow->next->next;
+        return head;
+    }
+};
