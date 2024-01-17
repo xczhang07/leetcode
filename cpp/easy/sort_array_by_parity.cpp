afcb829333b06786dc0216996e78ee370765a70c
xc-zhang <xczhang07@gmail.com>
2024-01-16 22:05:35 -0800

    Update sort_array_by_parity.cpp

--- cpp/easy/sort_array_by_parity.cpp
+++ cpp/easy/sort_array_by_parity.cpp
@@ -20,3 +20,25 @@ easy algorithm issue.
 Time Complexity is: O(n)
 Space Complexity is: O(n)
 */
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        if(nums.size() <= 1) {
+            return nums;
+        }
+        int even_idx = 0;
+        for(int i = 0; i < nums.size(); ++i) {
+            if(nums[i]%2 == 0) {
+                int tmp = nums[i];
+                nums[i] = nums[even_idx];
+                nums[even_idx] = tmp;
+                ++even_idx;
+            }
+        }
+        return nums;
+    }
+};
+
+Time Complexity: O(n)
+Space Complexity: O(1)
