eaf335a6922b0031ec8b6235787350a88e06ff4e
xc-zhang <xczhang07@gmail.com>
2024-01-28 17:30:41 -0800

    Update minimum_window_substring.cpp

--- cpp/hard/minimum_window_substring.cpp
+++ cpp/hard/minimum_window_substring.cpp
@@ -1,29 +1,42 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        string ret = "";
-        int cnt = 0;
-        int left = 0;
+        int m = s.size();
+        int n = t.size();
+        if(n > m) {
+            return "";
+        }
+        string ret;
         int min_len = INT_MAX;
-        unordered_map<char, int> letterCnt;
-        for(char c : t)
-            letterCnt[c]++;
-        for(int i = 0; i < s.size(); ++i)
-        {
-            --letterCnt[s[i]];
-            if(letterCnt[s[i]] >= 0)
-                cnt++;
-            while(cnt == t.size())
-            {
-                if(i-left+1 < min_len)
+        unordered_map<char, int> letter_map;
+        for(char c: t) {
+            letter_map[c]++;
+        }
+        int start = 0;
+        int cnt = 0;
+        for(int i = 0; i < s.size(); ++i) {
+            letter_map[s[i]]--;
+            if(letter_map[s[i]] >= 0) {
+                ++cnt;
+            }
+            
+            while(cnt == n) {
+                // Cover the memory limit test case error.
+                while(letter_map[s[start]]+1 <= 0) 
                 {
-                    min_len = i - left + 1;
-                    ret = s.substr(left, min_len);
+                    letter_map[s[start]]++;
+                    ++start;
                 }
-                letterCnt[s[left]]++;
-                if(letterCnt[s[left]] > 0)
-                    cnt--;
-                left++;
+                int tmp_len = i - start + 1;
+                if(tmp_len < min_len) {
+                    min_len = tmp_len;
+                    ret = s.substr(start, min_len);
+                }
+                letter_map[s[start]]++;
+                if(letter_map[s[start]] > 0) {
+                    --cnt;
+                }
+                ++start;
             }
         }
         return ret;
@@ -36,3 +49,6 @@ Conclusion: a hard level issue on leetcode, using sliding window concept to reso
 Time Complexity is: O(n)
 Space Complexity is: O(1)
 */
+
+
+
