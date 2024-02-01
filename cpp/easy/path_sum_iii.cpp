1676884610854f40273b566f764462f1ed75bb28
xc-zhang <xczhang07@gmail.com>
2024-02-01 00:59:58 -0800

    Update path_sum_iii.cpp

--- cpp/easy/path_sum_iii.cpp
+++ cpp/easy/path_sum_iii.cpp
@@ -31,6 +31,49 @@ public:
     }
 };
 
+//Non-overflow solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int ret = 0;
+
+    int pathSum(TreeNode* root, int sum) {
+       if(!root) {
+           return 0;
+       }
+       long sum_l = static_cast<long>(sum);
+       dfs(root, sum_l);
+       pathSum(root->left, sum_l);
+       pathSum(root->right, sum_l);
+       return ret;
+    }
+
+    void dfs(TreeNode* node, long sum) {
+        if(!node) {
+            return;
+        }
+        sum -= node->val;
+        if(sum == 0) {
+            ++ret;
+        }
+        dfs(node->left, sum);
+        dfs(node->right, sum);
+    }
+
+   
+};
+
 
 Conclusion: an easy algorithm issue of leetcode, path sum, dfs tree traverse. very standard typical issue.
 
