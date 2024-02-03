8982684b68fc46ac8671778f2b20ac01dda573fd
xc-zhang <xczhang07@gmail.com>
2024-02-03 11:55:11 -0800

    Update find_peak_element.cpp

--- cpp/middle/find_peak_element.cpp
+++ cpp/middle/find_peak_element.cpp
@@ -31,3 +31,28 @@ int findPeakElement(vector<int>& nums) {
     
     Time Complexity: O(logn)
     Space Complexity: O(1)
+
+A simple solution:
+Reference doc is: https://medium.com/nerd-for-tech/leetcode-find-peak-element-c5c867ae0d9c
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        if(nums.size() == 0) {
+            return -1;
+        }
+        if(nums.size() == 1) {
+            return 0;
+        }
+        int l = 0, r = nums.size()-1;
+        while(l < r) {
+            int m = l + (r-l)/2;
+            if(nums[m] < nums[m+1]) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        return l;
+    }
+};
