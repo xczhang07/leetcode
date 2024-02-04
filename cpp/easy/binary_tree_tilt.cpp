73832e9f5941b684928cb3bf5a017e165cf4db09
xc-zhang <xczhang07@gmail.com>
2024-02-04 08:58:25 -0800

    Update binary_tree_tilt.cpp

--- cpp/easy/binary_tree_tilt.cpp
+++ cpp/easy/binary_tree_tilt.cpp
@@ -30,3 +30,44 @@ public:
 
 Conclusion:
 easy, dfs, postorder
+
+
+Another Solution:
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+
+    void subTreeSum(TreeNode* root, int& sum) {
+        if(root == NULL) {
+            return;
+        }
+        sum += root->val;
+        subTreeSum(root->left, sum);
+        subTreeSum(root->right, sum);
+        return;
+    }
+
+    int findTilt(TreeNode* root) {
+        if(root == NULL) {
+            return 0;
+        }
+        int left_sum = 0, right_sum = 0;
+        subTreeSum(root->left, left_sum);
+        subTreeSum(root->right, right_sum);
+        sum += abs(left_sum-right_sum);
+        findTilt(root->left);
+        findTilt(root->right);
+        return sum;
+    }
+};
