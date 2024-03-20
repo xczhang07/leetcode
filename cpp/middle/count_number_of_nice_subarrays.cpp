52d7dc8d314dbcbfc8c76f920f080acaad2a26fd
xc-zhang <xczhang07@gmail.com>
2024-03-20 14:52:37 -0700

    Update count_number_of_nice_subarrays.cpp

--- cpp/middle/count_number_of_nice_subarrays.cpp
+++ cpp/middle/count_number_of_nice_subarrays.cpp
@@ -30,3 +30,33 @@ public:
         return ret;
     }
 };
+
+
+Using atMost k algorithm to resolve this issue
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return atMost(nums, k) - atMost(nums, k-1);
+    }
+
+    int atMost(vector<int>& nums, int k) {
+        int i = 0, j = 0;
+        int oddCnt = 0;
+        int ret = 0;
+        while(j < nums.size()) {
+            // detect an odd number.
+            if(nums[j] % 2 == 1) {
+                oddCnt++;
+            }
+            while(oddCnt > k) {
+                if(nums[i]%2 == 1) {
+                    --oddCnt;
+                }
+                ++i;
+            }
+            ret += j-i+1;
+            ++j;
+        }
+        return ret;
+    }
+};
