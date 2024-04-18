faff27d630a5ae735dd01f39ba5512f1f85d8169
xc-zhang <xczhang07@gmail.com>
2024-04-18 15:26:44 -0700

    Update k_closest_points_to_origin.cpp

--- cpp/easy/k_closest_points_to_origin.cpp
+++ cpp/easy/k_closest_points_to_origin.cpp
@@ -35,6 +35,29 @@ public:
     }
 };
 
+// An short alternative solution as following:
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<vector<int>> ret;
+        priority_queue<pair<int, int>> pq; // stores the distance and idx of the point.
+        for(int i = 0; i < points.size(); ++i) {
+            int dist = points[i][0]*points[i][0] + points[i][1]*points[i][1];
+            pq.push({dist, i});
+            if(pq.size() > k) {
+                pq.pop();
+            }
+        }
+        while(!pq.empty()) {
+            auto top = pq.top();
+            pq.pop();
+            ret.push_back(points[top.second]);
+        }
+        return ret;
+    }
+};
+
 
 Time Complexity is: O(nlogk)
 Space Complexity is: O(k)
