84c8ade4884cf764de627faa0ca83214a5cb49cb
xc-zhang <xczhang07@gmail.com>
2025-04-06 14:17:27 -0700

    Update coin_change.cpp
    Added the BFS version

--- cpp/middle/coin_change.cpp
+++ cpp/middle/coin_change.cpp
@@ -27,3 +27,41 @@ dp[i] = min(dp[i], dp[i-coins[j]] + 1) coins[j] is less or equal to i.
 Time Complexity is: O(n)
 Space Complexity is: O(n)
 */
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        if(amount <= 0) {
+            return 0;
+        }
+        sort(coins.begin(), coins.end());
+        queue<int> q;
+        vector<bool> visited(amount+1, 0);
+        visited[amount] = true;
+        q.push(amount);
+        int steps = 1;
+        while(!q.empty()) {
+            int size = q.size();
+            while(size) {
+                int money = q.front();
+                q.pop();
+                for(int c: coins) {
+                    int rest = money-c;
+                    if(rest == 0) {
+                        return steps;
+                    }
+                    if(rest < 0) {
+                        break;
+                    }
+                    if(!visited[rest]) {
+                        visited[rest] = true;
+                        q.push(rest);
+                    }
+                }
+                size -= 1;
+            }
+            steps += 1;
+        }
+        return -1;
+    }
+};
