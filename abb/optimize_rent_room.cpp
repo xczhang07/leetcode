3bdf6b884d782b3aa1b03746478c00395428797e
xc-zhang <xczhang07@gmail.com>
2025-08-27 00:13:57 -0700

    Update optimize_rent_room.cpp

--- abb/optimize_rent_room.cpp
+++ abb/optimize_rent_room.cpp
@@ -2,6 +2,9 @@
 代码是一个回溯题，不过题目描述套用了一些租房的概念，大意是一个group的人去旅游，要把他们放在同一个区域的房子里，
 怎么做到使用最少的房间，每个房间有可容纳人数和所在区域，解法就是先遍历出目标区域的房间列表，然后回溯所有房间，
 找出最优解。实际上就是一个正数数组里找最少的数凑成一个大于目标数的问题。
+这道题需要满足的条件：
+1. 要求剩余的床数最少。
+2. 在剩余床数最少的条件下，要求剩余的房间最少。
 
 //
 //  optimize_rent_room.h
@@ -59,6 +62,44 @@ vector<int> optimizeRentRoom(vector<int>& rooms, int& personNum) {
         }
     }
     return ret;
+}
+
+
+
+// Version 2
+// version 2: no unordered_map is required, using another two variables to save the space complexity.
+void backTrackingVer2(vector<int>& rooms, int& personNum, int idx, int totalBedNum, vector<int> tmpRentRooms, int& minLeftBedNum, vector<int>& ret) {
+    if(totalBedNum >= personNum) {
+        if(totalBedNum - personNum < minLeftBedNum) {
+            minLeftBedNum = totalBedNum-personNum;
+            ret = tmpRentRooms;
+        } else if (totalBedNum-personNum == minLeftBedNum){
+            if(tmpRentRooms.size() < ret.size()) {
+                ret = tmpRentRooms;
+            }
+        }
+        return;
+    }
+    for(int i = idx; i < rooms.size(); ++i) {
+        totalBedNum += rooms[i];
+        tmpRentRooms.push_back(i); // store the room idx instead of the room capacity.
+        backTrackingVer2(rooms, personNum, i+1, totalBedNum, tmpRentRooms, minLeftBedNum, ret);
+        tmpRentRooms.pop_back();
+        totalBedNum -= rooms[i];
+    }
+}
+
+vector<int> optimizeRentRoomVer2(vector<int>& rooms, int& personNum) {
+    if(rooms.size() == 0 or personNum == 0) {
+        return {};
+    }
+    int idx = 0;
+    vector<int> tmpRentRooms;
+    vector<int> optimalRentRooms;
+    int minLeftBedNum = INT_MAX;
+    int totalBedNum = 0;
+    backTrackingVer2(rooms, personNum, 0, totalBedNum, tmpRentRooms, minLeftBedNum, optimalRentRooms);
+    return optimalRentRooms;
 }
 
 
