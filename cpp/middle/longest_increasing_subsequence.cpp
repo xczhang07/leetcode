784accbdc9208257fceac2a738601dda80f8609a
xc-zhang <xczhang07@gmail.com>
2025-09-01 17:28:16 -0700

    Update longest_increasing_subsequence.cpp

--- cpp/middle/longest_increasing_subsequence.cpp
+++ cpp/middle/longest_increasing_subsequence.cpp
@@ -24,3 +24,42 @@ Dynamic Programming solution.
 Time Complexity is: O(n^2)
 Space Complexity: O(n)
 */
+
+// nlog(n) solution with binary search
+
+class Solution {
+public:
+    int binarySearch(vector<int>& nums, int num) {
+        int l = 0;
+        int r = nums.size()-1;
+        int idx = 0;
+        while(l <= r) {
+            int m = l + (r-l)/2;
+            if(nums[m] == num) {
+                return m;
+            } else if(nums[m] > num) {
+                idx = m;
+                r = m-1;
+            } else {
+                l = m+1;
+            }
+        }
+        return idx;
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.size() <= 1) {
+            return nums.size();
+        }
+        vector<int> ret;
+        for(int num: nums) {
+            if(ret.empty() or ret.back() < num) {
+                ret.push_back(num);
+            } else {
+                int idx = binarySearch(ret, num);
+                ret[idx] = num;
+            }
+        }
+        return ret.size();
+    }
+};
